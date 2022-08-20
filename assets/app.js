97093a458c2c8eac03421dbc450dfffb95ed61e2
Alex S <alexrs84@users.noreply.github.com>
2022-08-20 01:54:11 +0000

    js crypto tests

--- assets/app.js
+++ assets/app.js
@@ -175,3 +175,217 @@ $(function() {
 
     $('textarea.autogrow').css('overflow', 'hidden').autogrow();
 });
+
+
+
+function randomString(length) {
+    var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var i;
+    var result = "";
+    var isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
+
+    if(window.crypto && window.crypto.getRandomValues){
+        var values = new Uint32Array(length);
+        window.crypto.getRandomValues(values);
+        for(i=0; i<length; i++) {
+            result += charset[values[i] % charset.length];
+        }
+        return result;
+
+    } else if(isOpera)//Opera's Math.random is secure, see http://lists.w3.org/Archives/Public/public-webcrypto/2013Jan/0063.html
+    {
+        for(i=0; i<length; i++) {
+            result += charset[Math.floor(Math.random()*charset.length)];
+        }
+        return result;
+    }
+    else throw new Error("Your browser sucks and can't generate secure random numbers");
+}
+
+
+$('#container-note-test #btn-encrypt').on('click', function(){
+
+    //var newKey = window.crypto.randomUUID().replace('-', '');
+
+    var newKey = randomString(16);
+    $('#tst-key').text(newKey);
+
+    var sourceText = $('#tst-source').text();
+    
+
+    var keyData = str2ab(newKey);
+
+
+    var iv = new Uint8Array(16).buffer;
+    var ciphertext = b642ab("i4+WxNH8XYMnAm7RsRkfOw==");
+
+    (async () => {
+        var encrypted = await encrypt(sourceText);
+
+        $('#tst-encrypted').text( ab2b64(encrypted) );
+        //
+
+        var decrypted = await decrypt( b642ab($('#tst-encrypted').text()) );
+        var decryptedText = ab2str(decrypted);
+
+        $('#tst-decrypted').text(decryptedText);
+
+        console.log('ab2str decrypted', decryptedText);
+        var decryptedStr = new TextDecoder().decode(decrypted);
+        console.log('TextDecoder decrypted', decryptedStr);
+
+
+    })();
+
+    async function encrypt(sourceText) {
+        var key = await importKey();
+
+        try {
+            return await crypto.subtle.encrypt(
+            { name: "AES-CBC", iv: iv },
+            key,
+            str2ab(sourceText) //ArrayBuffer of data you want to encrypt
+            );
+        } catch (ex) {
+            console.error("Error: Name: ", ex.name, ", Message: ", ex.message);
+        }
+           
+    }
+
+    async function decrypt(encrypted) {
+        var key = await importKey();
+
+        try {
+            return await crypto.subtle.decrypt(
+            { name: "AES-CBC", iv: iv },
+            key,
+            encrypted
+            );
+        } catch (ex) {
+            console.error("Error: Name: ", ex.name, ", Message: ", ex.message);
+        }
+    }
+
+    async function importKey() {
+        var key = await crypto.subtle.importKey(
+            "raw",
+            keyData,
+            { name: "AES-CBC" },
+            true,
+            ["decrypt", "encrypt"]
+        );
+        return key;
+    }
+
+    // Helper -----------------------------------------------
+
+    // https://stackoverflow.com/a/11058858
+
+    function ab2str(buf) {
+        return String.fromCharCode.apply(null, new Uint16Array(buf));
+    }
+
+    function str2ab(str) {
+        const buf = new ArrayBuffer(str.length);
+        const bufView = new Uint8Array(buf);
+        for (let i = 0, strLen = str.length; i < strLen; i++) {
+            bufView[i] = str.charCodeAt(i);
+        }
+        return buf;
+    }
+
+    // https://stackoverflow.com/a/21797381/9014097
+    function b642ab(base64) {
+        var binary_string = window.atob(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
+
+    function ab2b64( buffer ) { //_arrayBufferToBase64
+        var binary = '';
+        var bytes = new Uint8Array( buffer );
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode( bytes[ i ] );
+        }
+        return window.btoa( binary );
+    }
+
+
+    // window.crypto.subtle.generateKey(
+    // {
+    //     name: "AES-GCM",
+    //     length: 256, //can be  128, 192, or 256
+    // },
+    //     false, //whether the key is extractable (i.e. can be used in exportKey)
+    //     ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
+    // )
+    // .then(function(key){
+    //     //returns a key object
+    //     console.log(key);
+    //     $('#tst-key').text(key);
+
+    //     var data = $('#tst-source').text();
+
+    //     window.crypto.subtle.encrypt(
+    //         {
+    //             name: "AES-GCM",
+
+    //             //Don't re-use initialization vectors!
+    //             //Always generate a new iv every time your encrypt!
+    //             //Recommended to use 12 bytes length
+    //             iv: window.crypto.getRandomValues(new Uint8Array(12)),
+
+    //             //Additional authentication data (optional)
+    //             additionalData: ArrayBuffer,
+
+    //             //Tag length (optional)
+    //             tagLength: 128, //can be 32, 64, 96, 104, 112, 120 or 128 (default)
+    //         },
+    //         key, //from generateKey or importKey above
+    //         data //ArrayBuffer of data you want to encrypt
+    //     )
+    //     .then(function(encrypted){
+    //         //returns an ArrayBuffer containing the encrypted data
+    //         console.log(new Uint8Array(encrypted));
+            
+    //         $('#tst-encrypted').text(btoa(new Uint8Array(encrypted)));
+
+
+    //         window.crypto.subtle.decrypt(
+    //             {
+    //                 name: "AES-GCM",
+    //                 iv: ArrayBuffer(12), //The initialization vector you used to encrypt
+    //                 additionalData: ArrayBuffer, //The addtionalData you used to encrypt (if any)
+    //                 tagLength: 128, //The tagLength you used to encrypt (if any)
+    //             },
+    //             key, //from generateKey or importKey above
+    //             data //ArrayBuffer of the data
+    //         )
+    //         .then(function(decrypted){
+    //             //returns an ArrayBuffer containing the decrypted data
+    //             console.log(new Uint8Array(decrypted));
+    //         })
+    //         .catch(function(err){
+    //             console.error(err);
+    //         });
+
+
+
+
+    //     })
+    //     .catch(function(err){
+    //         console.error(err);
+    //     });
+
+
+    // })
+    // .catch(function(err){
+    //     console.error(err);
+    // });
+
+});
