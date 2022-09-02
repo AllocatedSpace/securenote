b36d2556aa57d60e1f052db912f9c46e15a35902
Alex S <alexrs84@users.noreply.github.com>
2022-09-02 23:25:06 +0000

    build version in footer and sync script

--- sync.sh
+++ sync.sh
@@ -2,8 +2,12 @@
 
 
 # buildversion="$1"
-buildversion=$(git describe --tags --long)
+buildversion=$(git describe --tags --long | sed --expression='s/-/./g')
 builddate=$(git log -1 --format=%cd )
 
 echo "Writing $buildversion - $builddate to templates/build-id.html"
 echo "$buildversion - $builddate" > templates/build-id.html
+
+
+sudo /var/www/lib/sbin/fsync -d assets/* config/* migrations/* public/favicon/* public/images/* public/favicon.ico \
+ public/robots.txt public/sitemap.xml src/* templates/* tests/* translations/* package.json sync.sh webpack.config.js
