7f63825eefd7d091c3afa65b19152e4d6847a6f4
Alex S <alexrs84@users.noreply.github.com>
2022-09-03 10:37:02 +0000

    multi domain multi site

--- src/Controller/NotePageController.php
+++ src/Controller/NotePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class PageController extends AbstractController
+class NotePageController extends AbstractController
 {
 
     public function howItWorks(): Response
