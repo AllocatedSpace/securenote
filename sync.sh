0f0059037b2e0275ba05ec6bde131ac2a0d0d5ca
Alex S <alexrs84@users.noreply.github.com>
2024-12-17 13:07:03 -0600

    security fixes^C

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## on remote, yarn build && php bin/console cache:clear
+## on remote, yarn build && sudo -u www-data php bin/console cache:clear
 
 # buildversion="$1"
 buildversion=$(git describe --tags --long | sed --expression='s/-/./g')
@@ -12,4 +12,4 @@ echo "$buildversion - $builddate" > templates/build-id.html
 
 sudo /var/www/lib/sbin/fsync -c -d assets/* assets/**/* config/* config/**/* migrations/* migrations/*/** public/favicon/* \
  public/favicon/**/* public/images/* public/images/**/* public/favicon.ico \
- public/robots.txt public/sitemap.xml src/* src/**/* templates/**/* templates/* tests/* tests/**/* translations/* translations/**/* package.json sync.sh webpack.config.js
+ public/robots.txt public/sitemap.xml src/* src/**/* templates/**/* templates/* tests/* tests/**/* translations/* translations/**/* package.json sync.sh webpack.config.js composer.json composer.lock yarn.lock
